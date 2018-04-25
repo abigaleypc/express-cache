d8cdf28dca1466b3622b304822c42b994e91d748
Yu Abigale <yupeichun@kingsoft.com>
2018-04-25 23:42:28 +0800

    add main

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
+var serveStatic = require('serve-static')
 
-app.use('/public', express.static('public'))
+app.use('/public', express.static('public', {
+  maxAge: '1d',
+  setHeaders: setCustomCacheControl
+}))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+function setCustomCacheControl (res, path) {
+  if (serveStatic.mime.lookup(path) === 'text/html') {
+    // Custom Cache-Control for HTML files
+    res.setHeader('Cache-Control', 'public, max-age=1000')
+  }
+}
