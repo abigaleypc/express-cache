330069b41d8477d2f625e9a159eee003a8c5c7bc
Yu Abigale <abigaleypc@gmail.com>
2018-04-26 16:23:17 +0800

    update maxage

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
-console.log('------------------------------------');
-console.log("修改");
-console.log('------------------------------------');
+window.onload = function() {
+  let name = document.getElementById("name");
+  name.innerText = "abigale";
+  name.innerText = "AbigaleYu";  // 缓存时间内修改innerText
+};
